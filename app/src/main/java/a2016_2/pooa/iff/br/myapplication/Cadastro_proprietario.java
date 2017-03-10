4e17b409084e52b6541358fa12b0edb6dd7760bb
Caio Medina <caio__medina@hotmail.com>
2017-03-09 21:02:14 -0300

    primeiro commit

---
+++ app/src/main/java/a2016_2/pooa/iff/br/myapplication/Cadastro_proprietario.java
@@ -0,0 +1,13 @@
+package a2016_2.pooa.iff.br.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cadastro_proprietario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_proprietario);
+    }
+}
